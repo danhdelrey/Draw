e1bc3c651c6c32a5f159f795663725a9888190e1
danhdelrey <duonghoaidanh21092003@gmail.com>
2026-01-26 17:39:34 +0700

    Update AppModule.kt

--- composeApp/src/commonMain/kotlin/com/example/draw/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/example/draw/di/AppModule.kt
@@ -1,8 +1,10 @@
 package com.example.draw.di
 
+import androidx.compose.ui.input.key.Key.Companion.G
 import com.example.draw.data.datasource.local.DrawingRepository
 import com.example.draw.data.datasource.local.DrawingRepositoryImpl
 import com.example.draw.ui.feature.drawing.viewModel.DrawingScreenViewModel
+import com.example.draw.ui.feature.gallery.viewModel.GalleryScreenViewModel
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
@@ -26,5 +28,7 @@ expect val platformModule: Module
 
 val sharedModule = module {
     single<DrawingRepository> { DrawingRepositoryImpl(get()) }
+
+    factory { GalleryScreenViewModel(get()) }
     factory { DrawingScreenViewModel(get()) }
 }
