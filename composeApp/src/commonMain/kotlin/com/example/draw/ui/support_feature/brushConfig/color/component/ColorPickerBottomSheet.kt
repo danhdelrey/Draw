572b678033e0a8e514469dc2f52369a0639179e7
danhdelrey <duonghoaidanh21092003@gmail.com>
2026-01-28 17:13:44 +0700

    Update ColorPickerBottomSheet.kt

--- composeApp/src/commonMain/kotlin/com/example/draw/ui/support_feature/brushConfig/color/component/ColorPickerBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/com/example/draw/ui/support_feature/brushConfig/color/component/ColorPickerBottomSheet.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -38,6 +39,10 @@ fun ColorPickerBottomSheet(
     var currentBrush by remember { mutableStateOf(initialBrush) }
 
     val controller = rememberColorPickerController()
+
+    LaunchedEffect(Unit) {
+        controller.selectByColor(Color(initialBrush.colorArgb), false)
+    }
 
     CustomBottomSheet(
         onDismissRequest = {
