e7e78bea191e8d069dc0139d93505eef547f0e62
danhdelrey <duonghoaidanh21092003@gmail.com>
2026-01-29 11:46:19 +0700

    Update ToolPanel.kt

--- composeApp/src/commonMain/kotlin/com/example/draw/ui/common/component/ToolPanel.kt
+++ composeApp/src/commonMain/kotlin/com/example/draw/ui/common/component/ToolPanel.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.draw.data.model.brush.SolidBrush
@@ -27,7 +28,7 @@ fun ToolPanel(
     Row(
         modifier = Modifier
             .background(
-                color = MaterialTheme.colorScheme.surface
+                color = Color.Transparent
             )
             .padding(horizontal = 15.dp)
             .systemBarsPadding()
