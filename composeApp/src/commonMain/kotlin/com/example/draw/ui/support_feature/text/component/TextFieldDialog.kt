a66b9a78f7630298e786ecbf8c81c83f871f71ff
danhdelrey <duonghoaidanh21092003@gmail.com>
2026-02-05 09:38:12 +0700

    Create TextFieldDialog.kt

---
+++ composeApp/src/commonMain/kotlin/com/example/draw/ui/support_feature/text/component/TextFieldDialog.kt
@@ -0,0 +1,68 @@
+package com.example.draw.ui.support_feature.text.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.draw.ui.common.preview.PreviewComponent
+
+@Composable
+fun TextFieldDialog(
+    onDismissRequest: () -> Unit,
+    onConfirm: (String) -> Unit
+) {
+    var text by remember { mutableStateOf("") }
+
+    AlertDialog(
+        onDismissRequest = onDismissRequest,
+        shape = RectangleShape,
+        text = {
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                singleLine = true,
+                placeholder = { Text("Type something...") }
+            )
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirm(text)
+                    onDismissRequest()
+                },
+                enabled = text.isNotBlank()
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = onDismissRequest
+            ) {
+                Text("Cancel")
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextFieldDialogPreview() {
+    PreviewComponent {
+        TextFieldDialog(
+            onDismissRequest = {},
+            onConfirm = {}
+        )
+    }
+}
